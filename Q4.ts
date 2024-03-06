7e53f3d4de529623dc30942449118e5532209317
cheryl9012 <161365360+cheryl9012@users.noreply.github.com>
2024-03-06 21:25:13 +0500

    Add files via upload
    It is the fourth task of the assignment.

---
+++ Q4.ts
@@ -0,0 +1,5 @@
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+let famous_Person:string = "Toni Morrison"
+console.log(`${famous_Person},"You are your best thing"`);
